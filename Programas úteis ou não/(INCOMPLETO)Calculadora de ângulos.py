4c191084bd31ba20add8f03a16971afffd346b4c
Joao Marcos <jmcaco17@gmail.com>
2022-07-12 00:40:13 -0300

    msg

---
+++ Programas úteis ou não/(INCOMPLETO)Calculadora de ângulos.py
@@ -0,0 +1,8 @@
+import emoji
+print(emoji.emojize('Bem vindo a calculadora de ângulos :thumbsup:', use_aliases=True))
+print(emoji.emojize(
+    'Primeiramente é preciso definir oq desejas :thinking:', use_aliases=True))
+print('Digite 1 para receber informações de um angulo específico')
+print('Digite 2 para receber o valor do ângulo a partir do seu tangente')
+print('Digite 3 para receber o valor do ângulo a partir do seu seno')
+resposta1 = int(input('Digite 4 para receber o angulo a partir dos valores de cosseno')
